598c19b8b2e55d92c5d9766fe7907bcdf928df68
Ole Fritsch <olefritsch@icloud.com>
2016-08-14 09:43:04 +0200

    Polished UI and created TankPlayerController

---
+++ Source/BattleTanks/TankPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
